0503b4aef1f7b1a85a3e559caabd6dc088859a51
manjunathkumbar <manjunathkumbar393@gmail.com>
2020-04-05 15:08:48 +0530

    mern full stack

---
+++ testback/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    return res.send("your visting home page")
+})
+
+app.get('/signup', (req, res)=>{
+    return res.send("your visting login route")
+})
+
+app.get('/manjunath', (req, res)=>{
+    return res.send("Hi Manjunath")
+})
+
+app.get('/signout', (req, res)=>{
+    return res.send("Your are signed out successfully")
+})
+
+app.listen(port, ()=>{
+    console.log('server is up and running...')
+})
+
+// const port = 3000
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
